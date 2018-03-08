8fb2e34fb4be450318a227a27d18fbce8d479e3f
topo <contact@topoapp.in>
2018-03-08 17:26:43 +0530

    Initial commit

---
+++ app/src/main/java/service/mobiikey/safedel/com/trail/TrailNotifier.java
@@ -0,0 +1,12 @@
+package service.mobiikey.safedel.com.trail;
+
+/**
+ * Created by User on 3/7/2018.
+ */
+
+public interface TrailNotifier {
+
+    void onTrailSucess();
+
+    void onTrailFailure();
+}
